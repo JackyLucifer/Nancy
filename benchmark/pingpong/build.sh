a72a2e277e249b00f1c23b710678b9b2492624a7
HanYa <1848395727@qq.com>
2023-03-17 20:20:01 +0800

    chore: almost none

--- benchmark/pingpong/build.sh
+++ benchmark/pingpong/build.sh
@@ -1,2 +1,2 @@
-g++ -std=c++11 -O3 -I ../../include server_A.cc ../../src/signal.cc -o server
-g++ -std=c++11 -O3 -I ../../include client_B.cc ../../src/signal.cc -lpthread -o client
+g++ -std=c++11 -O3 -I ../../include server.cc ../../src/signal.cc -o server
+g++ -std=c++11 -O3 -I ../../include client.cc ../../src/signal.cc -lpthread -o client
--- demo/echo/echo_server.cc
+++ demo/echo/echo_server.cc
@@ -1,8 +1,7 @@
 #include "nancy/net/tcp_server.h"
-using namespace nc;
 
 int main() {
-    net::tcp_server serv(net::localhost);  // port: 9090
+    nc::net::tcp_server serv(nc::net::localhost);  // port: 9090
     const int buf_sz = 1025;
     char buf[buf_sz];
     serv.set_readable_cb([&](int fd) {
@@ -10,7 +9,7 @@ int main() {
         while ((rcv = read(fd, buf, buf_sz - 1)) > 0) {
             write(fd, buf, rcv);
         }
-        serv.add_event(fd, net::event::readable);
+        serv.add_event(fd, nc::net::event::readable);
     });
     serv.activate();
 }
